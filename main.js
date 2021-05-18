67021e6f677eceb9fc86718dbbddd065f26ad3b9
SHARMAMANNAT <83449796+SHARMAMANNAT@users.noreply.github.com>
2021-05-18 21:41:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var  i =  0;
+function update()
+
+{
+    i++;
+    var number_of_family_members_in_array  =  5
+    if(i>number_of_family_members_in_array)
+{
+
+
+i=0;
+
+}
+
+var updatedImage   =   image[i];
+var updatedName   =   names[i];
+
+document.getElementById("family_member_image").src  =  updatedImage;
+document.getElementById("family_member_name").innerHTML  =  updatedName;
+}
